6265f85427eaea627856d9b765912286a4345543
Eldrish <eldrish.ramosperez@gmail.com>
2025-05-31 00:11:17 -0700

    Routes not working for some reason

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import CandidateSearch from './pages/CandidateSearch';
 import SavedCandidates from './pages/SavedCandidates';
 
 const App: React.FC = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<CandidateSearch />} />
-      <Route path="/saved" element={<SavedCandidates />} />
-    </Routes>
-  </Router>
+  <Routes>
+    <Route path="/" element={<CandidateSearch />} />
+    <Route path="/saved" element={<SavedCandidates />} />
+  </Routes>
 );
 
 export default App;
